70514dee16bf6bf71f758c1d9b13909314a3e814
kgtc1208 <68916479+kgtc1208@users.noreply.github.com>
2020-08-02 00:57:29 +0800

    Update Recipes.js

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,23 +1,10 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faStar} from '@fortawesome/free-solid-svg-icons';
 
 function Recipe(props){
-    const [saved, setSaved] = useState(false)
-    var issaved = '';
-    if(saved){
-        issaved = 'Saved';
-    }
-
-    const getSiblings = e => {
-        const id = e.target.parentNode.className;
-        console.log(id)
-    }
-
     return(
         <div id = {props.name} className="recipe-container">
-            <div className="recipe-save" onClick={getSiblings}><FontAwesomeIcon icon={faStar} /></div>
-            <div className="recipe-issaved">{issaved}</div>
             <div className="recipe-image"><img src={props.img}></img></div>
             <div className="recipe-name">{props.name}</div>
             <div className="recipe-calories">Calories: {props.calories}</div>
